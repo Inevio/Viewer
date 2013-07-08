c21a6fec442d9c5991dbfd4b9228094cda753799
Robby2012 <robby.upm@gmail.com>
2013-07-08 16:32:14 +0200

    weeVisor - Now accepts urls

--- static/script.js
+++ static/script.js
@@ -6,69 +6,102 @@ wz.app.addScript( 6, 'common', function( win, app, lang, params ){
     var desktop = $( '#wz-desktop' );
     var resize  = 0;
 
+    var loadImage = function( imgWidth, imgHeight ){
+
+        var scale      = 1;
+        var niceLimit  = 100;
+        
+        if( imgHeight > desktop.height() - niceLimit ){
+            scale = ( desktop.height() - niceLimit ) / imgHeight;
+        }
+        
+        if( imgWidth > desktop.width() - niceLimit ){
+            
+            var tmpscale = ( desktop.width() - niceLimit ) / imgWidth;
+            
+            if( tmpscale < scale ){
+                scale = tmpscale;
+            }
+            
+        }
+
+        win.css({
+
+            x : ( $( window ).width() / 2 ) - ( parseInt( imgWidth * scale, 10 ) / 2 ) - 96,
+            y : ( $( window ).height() / 2 ) - ( parseInt( imgHeight * scale, 10 ) / 2 )
+
+        });
+
+        win.transition({ opacity : 1 }, 400 );
+
+        win
+            .animate({
+
+                width  : parseInt( imgWidth * scale, 10 ),
+                height : parseInt( imgHeight * scale, 10 ) + winBar.outerHeight()
+
+            }, 250 );
+
+        if( params[0] === 'url' ){
+
+            img
+                .css( 'scale', scale )
+                .fadeIn();
+
+        }else{
+
+            img
+                .on( 'load', function(){
+
+                    img
+                        .css( 'scale', scale )
+                        .fadeIn();
+
+                });
+
+        }
+
+    }
+
     win.addClass( 'wz-dragger' );
-    win.css( 'display', 'none' );
+    win.css( 'opacity', 0 );
 
     win
 
     .on( 'app-param', function( error, params ){
 
-        if( params ){
-            
-            wz.structure( params[ 0 ], function( error, structure ){
+        if( params[0] === 'url' ){
+
+            img
+                .hide()
+                .attr( 'src', params[1] )
+                .on( 'load', function(){
+
+                    var imgWidth  = img.width();
+                    var imgHeight = img.height();
+
+                    loadImage( imgWidth, imgHeight );
+
+                });
+
+        }else if( params ){
+
+            wz.structure( params[1], function( error, structure ){
 
                 if( error ){
                     alert( error );
                     return false;
                 }
 
-                var imgHeight  = structure.metadata.exif.height;
-                var imgWidth   = structure.metadata.exif.width;
-                var scale      = 1;
-                var niceLimit  = 100;
-                
-                if( imgHeight > desktop.height() - niceLimit ){
-                    scale = ( desktop.height() - niceLimit ) / imgHeight;
-                }
-                
-                if( imgWidth > desktop.width() - niceLimit ){
-                    
-                    var tmpscale = ( desktop.width() - niceLimit ) / imgWidth;
-                    
-                    if( tmpscale < scale ){
-                        scale = tmpscale;
-                    }
-                    
-                }
-
-                win.css({
-
-                    x : ( $( window ).width() / 2 ) - ( parseInt( imgWidth * scale, 10 ) / 2 ) - 96,
-                    y : ( $( window ).height() / 2 ) - ( parseInt( imgHeight * scale, 10 ) / 2 )
-
-                })
-
-                win.fadeIn();
-
-                win
-                    .animate({
-
-                        width  : parseInt( imgWidth * scale, 10 ),
-                        height : parseInt( imgHeight * scale, 10 ) + winBar.outerHeight()
-
-                    }, 250 );
+                var imgWidth  = structure.metadata.exif.width;
+                var imgHeight = structure.metadata.exif.height;
 
                 img
                     .hide()
-                    .attr( 'src', structure.thumbnails.original )
-                    .on( 'load', function(){
+                    .attr( 'src', structure.thumbnails.original );
 
-                        img
-                            .css( 'scale', scale )
-                            .fadeIn();
-
-                    });
-
+                loadImage( imgWidth, imgHeight );
+                
             });
 
         }
